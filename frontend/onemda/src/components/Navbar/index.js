fd250dd90fb1558323988e2177220420c054cf25
karkran <nikhil.karkra@anz.com>
2018-11-24 13:28:12 +1100

    setting up bootstrap and router

---
+++ frontend/onemda/src/components/Navbar/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar =()=> {
+  return (
+    <nav className="navbar navbar-expand-sm bg-dark navbar-dark">
+      <a className="navbar-brand" href="#" alt="Onmeda logo">Onmeda</a>
+    </nav>
+  )
+}
+
+export default Navbar;
